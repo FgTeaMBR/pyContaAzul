5b2af8e2ef5793917062227961428387123589ec
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2022-03-17 00:27:58 -0300

    readme

--- auth/oauth/constants.py
+++ auth/oauth/constants.py
@@ -2,7 +2,7 @@ import os
 import urllib.parse
 
 
-CLIENT_SECRET = 'F2cf0dV9QjrSEj2XRUiNYvQ3A30Ar3wG'
-CLIENT_ID = '51bWuca2XQWNa18fLbWmZrGt3M3NvRTX'
+CLIENT_SECRET = ''
+CLIENT_ID = ''
 REDIRECT_URI = urllib.parse.quote_plus("http://localhost:8000/auth/get-token")
 STATE = "random_string"
